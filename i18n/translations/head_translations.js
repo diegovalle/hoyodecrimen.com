0005851967ee47819ea9f4422ea18b05ab476c50
diego <git@diegovalle.net>
2024-09-03 08:14:52 -0600

    Better titles and descriptions

--- i18n/translations/head_translations.js
+++ i18n/translations/head_translations.js
@@ -2,24 +2,31 @@ export const translations = {
   en: {
     "/en/_title": "Mexico City Crime Rates and Statistics - HoyoDeCrimen",
     "/en/_description":
-      "Mexico City crime rates and violence maps. HoyoDeCrimen will help you find out how safe your neighborhood (colonia) is.",
-    "/en/cuadrantes-map_title": "Crime in Mexico City by Cuadrante",
+      "Mexico City murder rates and violence maps. Find out how dangerous or safe your neighborhood (colonia) is with our tools",
+      
+    "/en/cuadrantes-map_title": "Map of Crime in Mexico City by Cuadrante",
     "/en/cuadrantes-map_description":
       "Interactive map of crime rates in the cuadrantes of Mexico City",
-    "/en/neighborhoods_title": "Map of Homicides Rates in Mexico City by Neighborhood",
+      
+    "/en/neighborhoods_title": "Map of Neighborhood Homicide Rates in Mexico City",
     "/en/neighborhoods_description":
       "Interactive map of homicides rates in the different colonias of Mexico City. Find out which neighborhoods are safe with this map.",
-    "/en/sectores-map_title": "Crime Rate in Mexico City by Sector",
+      
+    "/en/sectores-map_title": "Map of Crime Rates in Mexico City by Sector",
     "/en/sectores-map_description":
       "Interactive map of crime rates in the different police sectors of Mexico City.",
+      
     "/en/map_title": "Mexico City Safety Map",
     "/en/map_description":
       "Map of crime incidents in Mexico City tagged with latitude/longitude, date and time of occurrence. Murders, robberies , carjackings, firearm lesions... ",
-    "/en/rates_title": "Maps of Crime Rates in Mexico City by Police Sector",
+      
+    "/en/rates_title": "Crime Rates by Police Sector in Mexico City ",
     "/en/rates_description": "Mexico City Crime Rates by Sector",
+    
     "/en/counts_title": "Crime Counts by Cuadrante in Mexico City",
     "/en/counts_description":
       "Maps of crime rates in Mexico City by cuadrante delictivo",
+      
     "/en/crime_title": "Charts of the Different Crimes in Mexico City",
     "/en/crime_description":
       "Small multiple charts of crime in all of Mexico City",
@@ -30,21 +37,27 @@ export const translations = {
     "/en/day_title": "Charts of Crime in Mexico City by Day of Week",
     "/en/day_description":
       "Small multiple charts of crime in Mexico City by day of week",
+      
     "/en/trends_title": "Mexico City Crime Trends",
     "/en/trends_description":
       "Is crime going up or down in Mexico City. Analyze crime trends with interactive charts",
+      
     "/en/changes_title": "Changes in Number of Crimes by Sector in Mexico City",
     "/en/changes_description":
       "Change in crime counts at the sector delictivo level in Mexico City. Is crime going up or down in yur area?",
+      
     "/en/compare-neighborhoods_title":
       "Compare Neighborhoods in Mexico City by Homicide Rate",
     "/en/compare-neighborhoods_description":
       "Compare crime by neighborhood in Mexico City. Where is the safest place to live? Crime in Roma, Condesa, Polanco...",
+      
     "/en/area-map_title": "Crime in your Area",
     "/en/area-map_description":
       "All crimes in Mexico City within 700 meters of where you are right now",
+      
     "/en/about_title": "Information About HoyoDeCrimen.com",
     "/en/about_description": "Information about HoyoDeCrimen.com",
+    
     "/en/privacy_title": "Privacy Policy",
     "/en/privacy_description":
       "Information on how HoyoDeCrimen.com protects your privacy",
@@ -53,6 +66,7 @@ export const translations = {
       "Crime by hour in Mexico City. At what time of the day are you most likely to be a victim of crime?",
     "/en/hour_description":
       "Charts of the number of crimes in Mexico City during each hour of the day",
+      
     "/en/underreporting_title": "Under-reporting crime in Mexico City",
     "/en/underreporting_description":
       "Analysis of crime under-reporting in Mexico City based on death certificates and victimization surveys",
@@ -60,25 +74,32 @@ export const translations = {
   es: {
     "/_title": "Crimen en la Ciudad de México - HoyoDeCrimen",
     "/_description":
-      "Índice delictivo de la Ciudad de México. Conoce las zonas rojas y colonias que debes evitar. Datos actualizados mensualmente",
+      "Conoce las zonas rojas y colonias que debes evitar en la Ciudad de México. Datos actualizados mensualmente",
+      
     "/cuadrantes-mapa/_title": "Mapa de Crímenes por Cuadrante",
     "/cuadrantes-mapa/_description":
-      "Mapa interactivo del número de crímenes en los cuadrantes de CDMX. Las zonas rojas de CDMX",
+      "Mapa interactivo del número de crímenes en los cuadrantes de CDMX.",
+      
     "/colonias/_title": "Mapa de Homicidios por Colonia en la CDMX",
     "/colonias/_description":
       "Mapa interactivo de tasas de homicidio en la Ciudad de México por colonia ¿Cuáles son las colonias más seguras y más peligrosas en CDMX?",
+      
     "/sectores-mapa/_title": "Mapa de Tasas de Crímenes en la CDMX por Sector",
     "/sectores-mapa/_description":
       "Mapa interactivo de tasas de crimen en la Ciudad de México por sector policíaco",
+      
     "/mapa/_title": "Mapa del Crimen en la Ciudad de México",
     "/mapa/_description":
       "Mapa de la inseguridad e incidentes criminales en la Ciudad de México con latitud, longitud, fecha y hora de ocurrencia",
+      
     "/tasas/_title": "Tasas de Criminalidad en la Ciudad de México por Sector",
     "/tasas/_description":
       "Mapas de las tasas de criminalidad en la Ciudad de México",
-    "/numero/_title": "Número de Crímenes en tu Cuadrante",
+      
+    "/numero/_title": "Número de Crímenes por Cuadrante en la CDMX",
     "/numero/_description":
       "Mapas de tasas delictivas por cuadrante en la Ciudad de México",
+      
     "/crimen/_title": "Gráficas de Diferentes Crímenes en la Ciudad de México",
     "/crimen/_description":
       "Gráficas del crimen en toda la Ciudad de México. Homicidios, Robo de Vehículo, Robo a Transeúnte con y sin Violencia",
@@ -90,29 +111,37 @@ export const translations = {
 
     "/hora/_title": "Gráficas de las Tasas de Crimen en CDMX por Hora del Día",
     "/hora/_description":
-      "Gráficas de las tasas de crimen en la Ciudad de México por hora del día",
+      "La actividad delictiva tiende a aumentar durante la noche, pero no siempre...",
+      
     "/dia/_title": "Gráficas del Crimen en CDMX por Día de la Semana",
     "/dia/_description":
-      "Gráficas del crimen en la Ciudad de México por día de la semana. Días más peligrosos en CDMX",
+      "En que días de la semana la actividad criminal tiende a aumentar, alcanzando su punto máximo...",
+      
     "/tendencias/_title": "Tendencias Criminales en la CDMX",
     "/tendencias/_description":
       "¿Está subiendo o bajando el número de homicidios que se cometen en la CDMX? ¿La pendiente de crecimiento es positiva o negativa?",
+      
     "/cambios/_title": "Cambio en Número de Crímenes por Sector",
     "/cambios/_description":
-      "Cambio en el número de crímenes en los sectores del Distrito Federal. Compara los últimos doce meses con el mismo periodo del año pasado",
+      "Cambio en el número de crímenes en los sectores del Distrito Federal. Compara los últimos doce meses con el mismo periodo del año previo",
+      
     "/comparar-colonias/_title":
-      "Compara la Tasa de Homicidio por Colonia en la CDMX",
+      "Compara Tasas de Homicidio por Colonia en la CDMX",
     "/comparar-colonias/_description":
       "Tasas de homicidio en las distintas colonias de la CDMX. Las zonas más seguras para vivir y trabajar en CDMX",
+      
     "/rumbo-mapa/_title": "Crimen por tu Rumbo",
     "/rumbo-mapa/_description": "Mapa de delitos por tu rumbo",
+    
     "/acerca/_title": "Acerca de HoyoDeCrimen.com",
     "/acerca/_description": "Información de la página HoyoDeCrimen.com",
+    
     "/privacidad/_title": "Política de Privacidad",
     "/privacidad/_description":
       "¿Cómo protegemos tus datos en HoyoDeCrimen.com?",
+      
     "/subregistro/_title": "Subregistro de delitos",
     "/subregistro/_description":
-      "Subregistro de delitos en CDMX. Incluyen un análisis con datos del INEGI y la  Encuesta Nacional de Victimización y Percepción sobre Seguridad Pública",
+      "Subregistro de delitos en CDMX. Incluye un análisis con datos del INEGI y la Encuesta Nacional de Victimización y Percepción sobre Seguridad Pública",
   },
 };
