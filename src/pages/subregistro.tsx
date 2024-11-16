8a8173db11ffa55430990789b33f289c0fe5ddaf
diego <git@diegovalle.net>
2024-11-16 16:01:42 -0600

    Better underreporting chart

--- src/pages/subregistro.tsx
+++ src/pages/subregistro.tsx
@@ -16,7 +16,7 @@ import deathData from "../assets/hom_acc_na.json";
 import NADeaths from "../assets/na_deaths.json";
 
 import * as echarts from "echarts/core";
-import { LineChart, BarChart } from "echarts/charts";
+import { LineChart, BarChart, ScatterChart } from "echarts/charts";
 import {
   GridComponent,
   TooltipComponent,
@@ -31,6 +31,7 @@ import ReactEChartsCore from "echarts-for-react/lib/core";
 import { YYYYmmddToDate15, getMonthYear } from "../components/utils";
 
 echarts.use([
+  ScatterChart,
   TitleComponent,
   TooltipComponent,
   GridComponent,
@@ -299,6 +300,147 @@ const SubregistroPage: React.FC<PageProps> = ({
     ],
   };
 
+  let corChartOption = {
+    animation: false,
+    legend: {},
+    title: {
+      text: "",
+      left: "center",
+      textStyle: {
+        fontFamily: "Roboto Condensed, Ubuntu, system-ui, sans-serif",
+        fontSize: 14,
+        fontWeight: "bold",
+      },
+    },
+    tooltip: {
+      trigger: "item",
+      axisPointer: {
+        animation: false,
+        label: {
+          backgroundColor: "#ccc",
+          borderColor: "#aaa",
+          borderWidth: 0.1,
+          shadowBlur: 0,
+          shadowOffsetX: 0,
+          shadowOffsetY: 0,
+          color: "#111",
+          fontFamily: "Roboto Condensed, Ubuntu, system-ui, sans-serif",
+        },
+      },
+      formatter: function (item) {
+        let date = YYYYmmddToDate15(item.data[2]);
+        let datestr = getMonthYear(date, language, "long", " ");
+        return (
+          `${datestr}<br/><br/>` +
+          '<span style="display:inline-block;margin-right:4px;border-radius:10px;width:10px;height:10px;background-color:' +
+          item.color +
+          ';"></span>' +
+          t("homicides") +
+          `: <b>` +
+          item.data[0] +
+          "</b><br/>" +
+          '<span style="display:inline-block;margin-right:4px;border-radius:10px;width:10px;height:10px;background-color:' +
+          item.color +
+          ';"></span>' +
+          t("accidents") +
+          ": <b>" +
+          item.data[1] +
+          "</b>"
+        );
+      },
+    },
+    grid: {
+      left: "5%",
+      right: "5%",
+      bottom: "10%",
+      top: "17%",
+      containLabel: true,
+    },
+    xAxis: {
+      name: t("number of homicides"),
+      nameLocation: "middle",
+      nameGap: 35,
+      nameTextStyle: {
+        fontFamily: "Roboto Condensed, Ubuntu, system-ui, sans-serif",
+        color: "#111",
+      },
+    },
+    yAxis: {
+      name: t("accidents with no mechanism of injury specified"),
+      nameLocation: "middle",
+      nameGap: 35,
+      nameTextStyle: {
+        fontFamily: "Roboto Condensed, Ubuntu, system-ui, sans-serif",
+        color: "#111",
+      },
+    },
+    series: [
+      {
+        name: t("January and February 2007"),
+        type: "scatter",
+        data: deathData
+          .filter((item) => {
+            if (item.yearmon === "2007-01-01" || item.yearmon === "2007-02-01")
+              return true;
+            if (item.yearmon.startsWith("2023")) return false;
+            else return false;
+          })
+          .map(function (item) {
+            return [item.homicides, item.na_accident, item.yearmon];
+          }),
+        itemStyle: {
+          color: "blue",
+          borderColor: "Black",
+          opacity: 1,
+        },
+        symbol: "circle",
+        symbolSize: 10,
+      },
+      {
+        name: t("others"),
+        type: "scatter",
+        data: deathData
+          .filter((item) => {
+            if (item.yearmon === "2007-01-01" || item.yearmon === "2007-02-01")
+              return false;
+            if (item.yearmon.startsWith("2023")) return false;
+            else return true;
+          })
+          .map(function (item) {
+            return [item.homicides, item.na_accident, item.yearmon];
+          }),
+        itemStyle: {
+          color: "#aaa",
+          borderColor: "Black",
+          opacity: 0.8,
+        },
+        symbol: "circle",
+        symbolSize: 10,
+      },
+      {
+        name: t("2023"),
+        type: "scatter",
+        data: deathData
+          .filter((item) => {
+            if (item.yearmon === "2007-01-01" || item.yearmon === "2007-02-01")
+              return false;
+            if (item.yearmon.startsWith("2023")) return true;
+            else return false;
+          })
+          .map(function (item) {
+            return [item.homicides, item.na_accident, item.yearmon];
+          }),
+        itemStyle: {
+          color: "red",
+          borderColor: "Black",
+          opacity: 1,
+        },
+        symbol: "circle",
+        symbolSize: 10,
+      },
+    ],
+  };
+
   let hogarChartOption = {
     animation: false,
     title: {
@@ -469,6 +611,39 @@ const SubregistroPage: React.FC<PageProps> = ({
           <ReactEChartsCore
             echarts={echarts}
             option={NAChartOption}
+            style={{ height: 400, width: "100%" }}
+            opts={{ locale: echarts.registerLocale(language) }}
+          />
+
+          <Space h="md" />
+        </Grid.Col>
+      </Grid>
+      <Grid pl={20}>
+        <Grid.Col
+          span={{ base: 12, md: 6, lg: 6 }}
+          offset={{ base: 0, md: 3, lg: 3 }}
+        >
+          <Space h="md" />
+          <Trans i18nKey="error2" />
+        </Grid.Col>
+      </Grid>{" "}
+      <Grid pl={20}>
+        <Grid.Col
+          span={{ base: 12, md: 10, lg: 10 }}
+          offset={{ base: 0, md: 1, lg: 1 }}
+        >
+          <Space h="xl" />
+          <Center>
+            {" "}
+            <Title order={2}>
+              {t(
+                "Correlation between homicides and accidents with no mechanism of injury specified"
+              )}
+            </Title>
+          </Center>
+          <ReactEChartsCore
+            echarts={echarts}
+            option={corChartOption}
             style={{ height: 400, width: "100%" }}
             opts={{ locale: echarts.registerLocale(language) }}
           />
